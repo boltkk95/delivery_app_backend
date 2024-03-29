ec5a7a13dd0c57e6a10acb8f1de5823756e4373d
boltkk95 <mdharshadkk@gmail.com>
2024-03-29 22:38:53 +0530

    swagger documentation added

--- src/server.js
+++ src/server.js
@@ -3,7 +3,13 @@ const bodyParser = require('body-parser');
 const pricingRoutes = require('./routes/pricingRoutes');
 const config = require('./config/config');
 
+// eslint-disable-next-line import/order, import/no-extraneous-dependencies
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+
 const app = express();
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Middleware
 app.use(bodyParser.json());
